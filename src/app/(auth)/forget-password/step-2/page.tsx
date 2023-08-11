323f11f6cfe8bb79f861338b0525fb31e79662d8
hendrywilliam <hendriwilliam29@gmail.com>
2023-08-11 15:41:54 +0700

    fix typo

--- src/app/(auth)/forget-password/step-2/page.tsx
+++ src/app/(auth)/forget-password/step-2/page.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ForgetPasswordStepTwoForm from "@/components/forms/forget-password-step-2-form";
 
-export default function page() {
+export default function ForgetPasswordStepTwoPage() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
       <div className="border w-3/4 lg:w-1/2 p-4 rounded-md">
